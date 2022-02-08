c1da2109bbde22a6362a2e0d1c49113c59d53c90
coder-nivaan <78674761+coder-nivaan@users.noreply.github.com>
2022-02-08 13:40:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function startClarification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', 
+    modelReady)  
+}
+
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
